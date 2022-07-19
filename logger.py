12b8dd96b2728f636aa0d8cbc49ec086d85faa18
EC2 Default User <ec2-user@ip-172-31-15-202.eu-central-1.compute.internal>
2022-07-19 09:31:39 +0000

    commit

--- logger.py
+++ logger.py
@@ -5,11 +5,11 @@ from datetime import datetime
 logger = logging.getLogger('taskcat_opensearch')
 now = datetime.now()
 dt_string = now.strftime("%Y-%m-%d_%H%M%S")
-logging.basicConfig(filename='testcat-{}.log'.format(dt_string), level=logging.DEBUG)
+logging.basicConfig(filename='testcat-{}.log'.format(dt_string), level=logging.INFO)
 
 # create console handler and set level to info
 ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
+ch.setLevel(logging.INFO)
 
 # create formatter
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
